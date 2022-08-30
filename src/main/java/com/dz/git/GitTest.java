046906883c3cb225e1301c321f15196baa7cb609
Dz <Dz@666.com>
2022-08-30 17:21:18 +0800

    hot_fix commit source file

--- src/main/java/com/dz/git/GitTest.java
+++ src/main/java/com/dz/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello fix");
     }
 }
