33f5f7d7c8c14d7757457ac29b503f2e82e7cb82
Dz <Dz@666.com>
2022-08-30 17:25:03 +0800

    master_conflict commit source file

--- src/main/java/com/dz/git/GitTest.java
+++ src/main/java/com/dz/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("hello2");
         System.out.println("hello3");
-        System.out.println("hello fix");
+        System.out.println("hello fix_conflict");
     }
 }
